c2c43fa60ef69f608364e9d27d391ac7f990ca9b
Julio Cascalles <jcezarc@yahoo.com.br>
2020-06-24 22:33:01 -0300

    requirements.txt

---
+++ run.bat
@@ -0,0 +1,17 @@
+@echo off
+if(%1)==(front) goto front
+
+:back
+set NAFILABR_USER=root
+set NAFILABR_PASSWORD=
+cd backend
+start python app.py
+cd ..
+if(%1)==(back) goto fim
+
+:front
+cd frontend
+start ng serve --open --proxy-config proxy.conf.json
+cd ..
+
+:fim
